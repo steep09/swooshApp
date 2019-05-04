1881b57575eeb5cb1052f24ee2cc56842a626428
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-04 22:46:13 +0800

    added the auto-layout for the app

--- appSwoosh/appSwoosh/ViewController.swift
+++ appSwoosh/appSwoosh/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var bgImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    
 }
 
