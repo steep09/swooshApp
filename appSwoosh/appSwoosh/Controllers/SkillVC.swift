5de1f8d6ddb307786d751d97415e69ad91760b15
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-06 08:26:09 +0800

    Create viewController for all three pages

---
+++ appSwoosh/appSwoosh/Controllers/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  appSwoosh
+//
+//  Created by Stephenson Ang on 06/05/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
