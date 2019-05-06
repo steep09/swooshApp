81ec17d9836f28757b53efe650d8efd756e994e5
Stephenson Ang <stiph@Stephensons-MacBook-Pro.local>
2019-05-06 13:53:28 +0800

    add models and actions when button is clicked

--- appSwoosh/appSwoosh/Controllers/LeagueVC.swift
+++ appSwoosh/appSwoosh/Controllers/LeagueVC.swift
@@ -10,25 +10,35 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
     
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "SkillVCSegue", sender: self)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func onMenTapped(_ sender: Any) {
+        selectLeague(leagueType: "men")
     }
-    */
+    
+    @IBAction func onWoemTapped(_ sender: Any) {
+        selectLeague(leagueType: "women")
+    }
+    
+    @IBAction func onCoEdTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
     
 }
---
+++ appSwoosh/appSwoosh/Models/player.swift
@@ -0,0 +1,14 @@
+//
+//  player.swift
+//  appSwoosh
+//
+//  Created by Stephenson Ang on 06/05/2019.
+//  Copyright © 2019 Stephenson Ang. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
